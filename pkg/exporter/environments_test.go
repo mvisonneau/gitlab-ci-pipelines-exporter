64c5ca566c3d7dddf0aa9b6e1a8adb9ba8487d8f
Maxime VISONNEAU <maxime.visonneau@gmail.com>
2020-10-31 19:45:33 +0000

    Added some more tests which allowed to discover and fix an issue on pullEnvironmentMetrics func

--- pkg/exporter/environments_test.go
+++ pkg/exporter/environments_test.go
@@ -1,1 +1,164 @@
 package exporter
+
+import (
+	"fmt"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/mvisonneau/gitlab-ci-pipelines-exporter/pkg/schemas"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPullEnvironmentsFromProject(t *testing.T) {
+	resetGlobalValues()
+	mux, server := configureMockedGitlabClient()
+	defer server.Close()
+
+	mux.HandleFunc(fmt.Sprintf("/api/v4/projects/foo/environments"),
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `[{"name":"main"},{"id":1337,"name":"prod"}]`)
+		})
+
+	mux.HandleFunc("/api/v4/projects/foo/environments/1337",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `
+{
+	"id": 1,
+	"name": "foo",
+	"external_url": "https://foo.example.com",
+	"state": "available",
+	"last_deployment": {
+		"id": 2,
+		"iid": 30,
+		"ref": "bar",
+		"created_at": "2019-03-25T18:55:13.252Z",
+		"deployable": {
+			"status": "success",
+			"tag": false,
+			"duration": 21623.13423,
+			"user": {
+				"public_email": "foo@bar.com"
+			},
+			"commit": {
+				"short_id": "416d8ea1"
+			}
+		}
+	}
+}`)
+		})
+
+	assert.NoError(t, pullEnvironmentsFromProject(schemas.Project{Name: "foo"}))
+
+	storedEnvironments, _ := store.Environments()
+	expectedCreateAt, _ := time.Parse(time.RFC3339, "2019-03-25T18:55:13.252Z")
+	expectedEnvironments := schemas.Environments{
+		"3047620480": schemas.Environment{
+			ProjectName: "foo",
+			ID:          1337,
+			Name:        "foo",
+			ExternalURL: "https://foo.example.com",
+			Available:   true,
+			LatestDeployment: schemas.Deployment{
+				RefKind:       schemas.RefKindBranch,
+				RefName:       "bar",
+				AuthorEmail:   "foo@bar.com",
+				CreatedAt:     expectedCreateAt,
+				Duration:      21623 * time.Second,
+				CommitShortID: "416d8ea1",
+				Status:        "success",
+			},
+		},
+	}
+	assert.Equal(t, expectedEnvironments, storedEnvironments)
+}
+
+func TestPullEnvironmentMetricsSucceed(t *testing.T) {
+	resetGlobalValues()
+	mux, server := configureMockedGitlabClient()
+	defer server.Close()
+
+	mux.HandleFunc("/api/v4/projects/foo/environments/1",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `
+{
+	"id": 1,
+	"name": "prod",
+	"external_url": "https://foo.example.com",
+	"state": "available",
+	"last_deployment": {
+		"id": 2,
+		"iid": 30,
+		"ref": "bar",
+		"created_at": "2019-03-25T18:55:13.252Z",
+		"deployable": {
+			"status": "success",
+			"tag": false,
+			"duration": 21623.13423,
+			"user": {
+				"public_email": "foo@bar.com"
+			},
+			"commit": {
+				"short_id": "416d8ea1"
+			}
+		}
+	}
+}`)
+		})
+
+	mux.HandleFunc("/api/v4/projects/foo/repository/branches/bar",
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `
+{
+	"commit": {
+		"short_id": "416d8ea1",
+		"committed_date": "2019-03-25T18:55:13.252Z"
+	}
+}`)
+		})
+
+	env := schemas.Environment{
+		ProjectName: "foo",
+		Name:        "prod",
+		ID:          1,
+	}
+
+	// Metrics pull shall succeed
+	assert.NoError(t, pullEnvironmentMetrics(env))
+
+	// Check if all the metrics exist
+	metrics, _ := store.Metrics()
+	labels := map[string]string{
+		"project":     "foo",
+		"environment": "prod",
+	}
+
+	environmentBehindCommitsCount := schemas.Metric{
+		Kind:   schemas.MetricKindEnvironmentBehindCommitsCount,
+		Labels: labels,
+		Value:  0,
+	}
+	assert.Equal(t, environmentBehindCommitsCount, metrics[environmentBehindCommitsCount.Key()])
+
+	environmentBehindCommitsDurationSeconds := schemas.Metric{
+		Kind:   schemas.MetricKindEnvironmentBehindDurationSeconds,
+		Labels: labels,
+		Value:  0,
+	}
+	assert.Equal(t, environmentBehindCommitsDurationSeconds, metrics[environmentBehindCommitsDurationSeconds.Key()])
+
+	environmentDeploymentDurationSeconds := schemas.Metric{
+		Kind:   schemas.MetricKindEnvironmentDeploymentDurationSeconds,
+		Labels: labels,
+		Value:  21623,
+	}
+	assert.Equal(t, environmentDeploymentDurationSeconds, metrics[environmentDeploymentDurationSeconds.Key()])
+
+	labels["status"] = "success"
+	status := schemas.Metric{
+		Kind:   schemas.MetricKindEnvironmentDeploymentStatus,
+		Labels: labels,
+		Value:  1,
+	}
+	assert.Equal(t, status, metrics[status.Key()])
+}
