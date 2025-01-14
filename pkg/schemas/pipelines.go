mvisonneau/gitlab-ci-pipelines-exporter

commit e6cc3e68151d438d5b6324ade62b98731da54171

feat: detect pipeline with status success-with-warnings (#878)
Co-authored-by: Nicolas Boussuge <nboussuge@payplug.com>

pkg/schemas/pipelines.go
@@ -80,7 +80,7 @@ func NewPipeline(ctx context.Context, gp goGitlab.Pipeline) Pipeline {
 		DurationSeconds:       float64(gp.Duration),
 		QueuedDurationSeconds: float64(gp.QueuedDuration),
 		Source:                gp.Source,
-		Status:                gp.Status,
+		Status:                gp.DetailedStatus.Group,
 	}
 }
 

pkg/schemas/pipelines_test.go
@@ -14,29 +14,78 @@ func TestNewPipeline(t *testing.T) {
 	startedAt := time.Date(2020, 10, 1, 13, 5, 10, 0, time.UTC)
 	updatedAt := time.Date(2020, 10, 1, 13, 5, 50, 0, time.UTC)
 
-	gitlabPipeline := goGitlab.Pipeline{
-		ID:             21,
-		Coverage:       "25.6",
-		CreatedAt:      &createdAt,
-		StartedAt:      &startedAt,
-		UpdatedAt:      &updatedAt,
-		Duration:       15,
-		QueuedDuration: 5,
-		Source:         "schedule",
-		Status:         "running",
+	testCases := []struct {
+		status         string
+		detailedStatus goGitlab.DetailedStatus
+		expectedStatus string
+	}{
+		{
+			"running",
+			goGitlab.DetailedStatus{
+				Text:  "Running",
+				Label: "running",
+				Group: "running",
+			},
+			"running",
+		},
+		{
+			"success",
+			goGitlab.DetailedStatus{
+				Text:  "Passed",
+				Label: "passed",
+				Group: "success",
+			},
+			"success",
+		},
+		{
+			"canceled",
+			goGitlab.DetailedStatus{
+				Text:  "Canceled",
+				Label: "canceled",
+				Group: "canceled",
+			},
+			"canceled",
+		},
+		{
+			"success",
+			goGitlab.DetailedStatus{
+				Text:  "Warning",
+				Label: "passed with warnings",
+				Group: "success-with-warnings",
+			},
+			"success-with-warnings",
+		},
 	}
 
-	expectedPipeline := Pipeline{
-		ID:                    21,
-		Coverage:              25.6,
-		Timestamp:             1.60155755e+09,
-		DurationSeconds:       15,
-		QueuedDurationSeconds: 5,
-		Source:                "schedule",
-		Status:                "running",
+	for _, tc := range testCases {
+		t.Run(tc.status, func(t *testing.T) {
+			gitlabPipeline := goGitlab.Pipeline{
+				ID:             21,
+				Coverage:       "25.6",
+				CreatedAt:      &createdAt,
+				StartedAt:      &startedAt,
+				UpdatedAt:      &updatedAt,
+				Duration:       15,
+				QueuedDuration: 5,
+				Source:         "schedule",
+				Status:         tc.status,
+				DetailedStatus: &tc.detailedStatus,
+			}
+
+			expectedPipeline := Pipeline{
+				ID:                    21,
+				Coverage:              25.6,
+				Timestamp:             1.60155755e+09,
+				DurationSeconds:       15,
+				QueuedDurationSeconds: 5,
+				Source:                "schedule",
+				Status:                tc.expectedStatus,
+			}
+
+			assert.Equal(t, expectedPipeline, NewPipeline(context.Background(), gitlabPipeline))
+		})
 	}
 
-	assert.Equal(t, expectedPipeline, NewPipeline(context.Background(), gitlabPipeline))
 }
 
 func TestNewTestReport(t *testing.T) {

commit 0b58d19ddadf9b57d49370cb6f7e111f5170d848

fix(bug): fixed a nil pointer dereference error introduced with #878

pkg/schemas/pipelines.go
@@ -73,15 +73,22 @@ func NewPipeline(ctx context.Context, gp goGitlab.Pipeline) Pipeline {
 		timestamp = float64(gp.UpdatedAt.Unix())
 	}
 
-	return Pipeline{
+	pipeline := Pipeline{
 		ID:                    gp.ID,
 		Coverage:              coverage,
 		Timestamp:             timestamp,
 		DurationSeconds:       float64(gp.Duration),
 		QueuedDurationSeconds: float64(gp.QueuedDuration),
 		Source:                gp.Source,
-		Status:                gp.DetailedStatus.Group,
 	}
+
+	if gp.DetailedStatus != nil {
+		pipeline.Status = gp.DetailedStatus.Group
+	} else {
+		pipeline.Status = gp.Status
+	}
+
+	return pipeline
 }
 
 // NewTestReport ..